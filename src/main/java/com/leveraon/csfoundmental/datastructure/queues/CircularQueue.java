d640cf6a7ff58f992087e9d038d649dd2c5a098f
Yu Sang <yu.sang@cgi.com>
2024-07-29 12:01:35 -0300

    update example.

---
+++ src/main/java/com/leveraon/csfoundmental/datastructure/queues/CircularQueue.java
@@ -0,0 +1,9 @@
+package main.java.com.leveraon.csfoundmental.datastructure.queues;
+
+public interface CircularQueue<E> extends Queue<E> {
+    /**
+     * Rotates the front element of the queue to the back of the queue.
+     * This does nothing if the queue is empty.
+     */
+    void rotate();
+}
